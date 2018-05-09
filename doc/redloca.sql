5185d10df422313c904c1715c819de64d3852bcf
xordustz <xordustz@gmail.com>
2018-05-09 16:41:31 +0200

    modification dans l'arborescence
    remplacement du fichier slq

--- doc/redloca.sql
+++ doc/redloca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 08 mai 2018 à 14:39
+-- Généré le :  mer. 09 mai 2018 à 14:38
 -- Version du serveur :  5.7.21
 -- Version de PHP :  7.2.4
 
@@ -30,13 +30,13 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
-  `idcategorie` int(11) NOT NULL,
+  `idcategorie` int(11) NOT NULL AUTO_INCREMENT,
   `nomCategorie` varchar(45) NOT NULL,
   `prixCategorie` varchar(45) NOT NULL,
   `type_categories_idTypeCategorie` int(11) NOT NULL,
   PRIMARY KEY (`idcategorie`),
-  KEY `fk_categories_type_categories1_idx` (`type_categories_idTypeCategorie`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `fk_categories_type_categories1_idx1` (`type_categories_idTypeCategorie`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `categories`
@@ -80,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `reservation` (
 --
 
 INSERT INTO `reservation` (`dateDebut`, `dateFin`, `Vehicules_idVehicule`, `utilisateurs_idutilisateur`) VALUES
-('2018-05-08', '2018-05-10', 1, 1),
+('2018-05-05', '2018-05-08', 1, 1),
 ('2018-05-08', '2018-07-10', 5, 2),
 ('2018-05-08', '2018-06-07', 2, 3);
 
@@ -92,10 +92,10 @@ INSERT INTO `reservation` (`dateDebut`, `dateFin`, `Vehicules_idVehicule`, `util
 
 DROP TABLE IF EXISTS `type_categories`;
 CREATE TABLE IF NOT EXISTS `type_categories` (
-  `idTypeCategorie` int(11) NOT NULL,
+  `idTypeCategorie` int(11) NOT NULL AUTO_INCREMENT,
   `nomTypeCategorie` varchar(45) NOT NULL,
   PRIMARY KEY (`idTypeCategorie`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `type_categories`
@@ -117,9 +117,10 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `nom` varchar(45) NOT NULL,
   `prenom` varchar(45) NOT NULL,
   `dateNaissance` date NOT NULL,
-  `natel` varchar(12) DEFAULT NULL,
+  `natel` varchar(12) NOT NULL,
   `Email` varchar(45) NOT NULL,
   `Mdp` varchar(100) NOT NULL,
+  `type` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`idutilisateur`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
@@ -127,11 +128,11 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
 -- Déchargement des données de la table `utilisateurs`
 --
 
-INSERT INTO `utilisateurs` (`idutilisateur`, `nom`, `prenom`, `dateNaissance`, `natel`, `Email`, `Mdp`) VALUES
-(1, 'ryser', 'tom', '1998-03-03', '0798380065', 'tom.rsr@eduge.ch', '52d166edfc5ee6a6413333ea67be0eb40d467a72'),
-(2, 'nikxla', 'nikola', '1998-04-30', '0893111565', 'nikola.antnj@eduge.ch', '52d166edfc5ee6a6413333ea67be0eb40d467a72'),
-(3, 'djokovic', 'damjan', '1998-04-22', '04813461570', 'damjan.jvnvc@eduge.ch', '52d166edfc5ee6a6413333ea67be0eb40d467a72'),
-(4, 'ryry', 'tomtom', '1998-03-03', '+41798380069', 'tom.rsr@eduge.ch', '52d166edfc5ee6a6413333ea67be0eb40d467a72');
+INSERT INTO `utilisateurs` (`idutilisateur`, `nom`, `prenom`, `dateNaissance`, `natel`, `Email`, `Mdp`, `type`) VALUES
+(1, 'ryser', 'tom', '1998-03-03', '0798380065', 'tom.rsr@eduge.ch', '52d166edfc5ee6a6413333ea67be0eb40d467a72', 1),
+(2, 'nikxla', 'nikola', '1998-04-30', '0893111565', 'nikola.antnj@eduge.ch', '52d166edfc5ee6a6413333ea67be0eb40d467a72', 0),
+(3, 'djokovic', 'damjan', '1998-04-22', '04813461570', 'damjan.jvnvc@eduge.ch', '52d166edfc5ee6a6413333ea67be0eb40d467a72', 0),
+(4, 'ryry', 'tomtom', '1998-03-03', '+41798380069', 'tom.rsr@eduge.ch', '52d166edfc5ee6a6413333ea67be0eb40d467a72', 0);
 
 -- --------------------------------------------------------
 
@@ -150,25 +151,25 @@ CREATE TABLE IF NOT EXISTS `vehicules` (
   `image` varchar(45) NOT NULL,
   `dateDebutDisponibilite` varchar(45) NOT NULL,
   `dateFinDisponibilite` varchar(45) DEFAULT NULL,
-  `categories_idcategorie` int(11) NOT NULL,
   `utilisateurs_idutilisateur` int(11) NOT NULL,
+  `categories_idcategorie` int(11) NOT NULL,
   PRIMARY KEY (`idVehicule`),
   UNIQUE KEY `idVehicule_UNIQUE` (`idVehicule`),
-  KEY `fk_Vehicules_categories1_idx` (`categories_idcategorie`),
-  KEY `fk_Vehicules_utilisateurs1_idx` (`utilisateurs_idutilisateur`)
+  KEY `fk_vehicules_utilisateurs1_idx` (`utilisateurs_idutilisateur`),
+  KEY `fk_vehicules_categories1_idx` (`categories_idcategorie`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `vehicules`
 --
 
-INSERT INTO `vehicules` (`idVehicule`, `immatriculation`, `marque`, `modele`, `nbPlace`, `couleur`, `image`, `dateDebutDisponibilite`, `dateFinDisponibilite`, `categories_idcategorie`, `utilisateurs_idutilisateur`) VALUES
+INSERT INTO `vehicules` (`idVehicule`, `immatriculation`, `marque`, `modele`, `nbPlace`, `couleur`, `image`, `dateDebutDisponibilite`, `dateFinDisponibilite`, `utilisateurs_idutilisateur`, `categories_idcategorie`) VALUES
 (1, 'GE 999 999', 'audi', 'R8', 2, 'rouge', 'image', '2018-04-10', NULL, 1, 1),
-(2, 'GE 123 123', 'Yamaha', 'Fz6n', 2, 'noir', 'image', '2018-04-10', '2018-04-30', 14, 1),
-(3, 'GE 123 321', 'Yamaha', 'Fz6n', 2, 'noir', 'image', '2018-04-01', '2018-05-05', 14, 1),
-(4, 'GE 321 321', 'Yamaha', 'Fz6n', 2, 'noir', 'image', '2018-03-01', '2018-05-10', 14, 1),
+(2, 'GE 123 123', 'Yamaha', 'Fz6n', 2, 'noir', 'image', '2018-04-10', '2018-04-30', 1, 14),
+(3, 'GE 123 321', 'Yamaha', 'Fz6n', 2, 'noir', 'image', '2018-04-01', '2018-05-05', 1, 14),
+(4, 'GE 321 321', 'Yamaha', 'Fz6n', 2, 'noir', 'image', '2018-03-01', '2018-05-10', 1, 14),
 (5, 'GE 999 999', 'audi', 'R8', 2, 'rouge', 'image', '2018-05-08', NULL, 1, 1),
-(6, 'GE 456 789', 'Audi', 'R8', 2, 'Orange', 'image', '2018-05-08', NULL, 1, 2);
+(6, 'GE 456 789', 'Audi', 'R8', 2, 'Orange', 'image', '2018-08-08', NULL, 2, 1);
 
 --
 -- Contraintes pour les tables déchargées
@@ -191,8 +192,8 @@ ALTER TABLE `reservation`
 -- Contraintes pour la table `vehicules`
 --
 ALTER TABLE `vehicules`
-  ADD CONSTRAINT `fk_Vehicules_categories1` FOREIGN KEY (`categories_idcategorie`) REFERENCES `categories` (`idcategorie`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Vehicules_utilisateurs1` FOREIGN KEY (`utilisateurs_idutilisateur`) REFERENCES `utilisateurs` (`idutilisateur`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_vehicules_categories1` FOREIGN KEY (`categories_idcategorie`) REFERENCES `categories` (`idcategorie`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_vehicules_utilisateurs1` FOREIGN KEY (`utilisateurs_idutilisateur`) REFERENCES `utilisateurs` (`idutilisateur`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
